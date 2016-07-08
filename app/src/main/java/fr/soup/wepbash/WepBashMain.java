3e56a6ba341dec92ee2c5091705958a5f1bc3b5a
Moi4167 <ruffino.cyprien@gmail.com>
2016-07-08 15:47:16 +0200

    Project settings

---
+++ app/src/main/java/fr/soup/wepbash/WepBashMain.java
@@ -0,0 +1,9 @@
+package fr.soup.wepbash;
+
+import android.app.Application;
+
+/**
+ * Created by cyprien on 08/07/16.
+ */
+public class WepBashMain extends Application{
+}
---
+++ app/src/main/java/fr/soup/wepbash/WepBashMainActivity.java
@@ -0,0 +1,9 @@
+package fr.soup.wepbash;
+
+import android.app.Activity;
+
+/**
+ * Created by cyprien on 08/07/16.
+ */
+public class WepBashMainActivity extends Activity {
+}
