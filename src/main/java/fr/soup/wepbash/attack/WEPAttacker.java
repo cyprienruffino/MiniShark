1deb03595808b40dbdf60f1099a6e3a0a5116528
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2016-07-13 14:43:08 +0200

    Setup de gradle

--- src/main/java/fr/soup/wepbash/attack/WEPAttacker.java
+++ src/main/java/fr/soup/wepbash/attack/WEPAttacker.java
@@ -1,6 +1,8 @@
 package fr.soup.wepbash.attack;
 
 
+import org.jnetpcap.Pcap;
+
 /**
  * Created by cyprien on 12/07/16.
  */
@@ -14,7 +16,9 @@ public class WEPAttacker extends Attacker {
 
     @Override
     public void attack(AttackCallback callback) {
-
+        Pcap pcap = new Pcap();
+        byte[] pack = {0x0000};
+        pcap.sendPacket(pack);
         callback.succesCallback(ssid,key);
     }
 
